b248997d8a24601fa18027b09da7bf89a482aba9
Kishore Pashindla <kishore.pashindla@gmail.com>
2022-12-06 11:29:53 +0000

    second

--- src/main/java/com/ab/SpringBootMvcCiCd2Application.java
+++ src/main/java/com/ab/SpringBootMvcCiCd2Application.java
@@ -2,10 +2,9 @@ package com.ab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootMvcCiCd2Application  extends SpringBootServletInitializer {
+public class SpringBootMvcCiCd2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcCiCd2Application.class, args);
