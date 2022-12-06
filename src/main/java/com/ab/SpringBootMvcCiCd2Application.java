e37d2c28255039a3f3b2eb1531ca17cb3b0fc0e3
Kishore Pashindla <kishore.pashindla@gmail.com>
2022-12-06 11:34:11 +0000

    second

--- src/main/java/com/ab/SpringBootMvcCiCd2Application.java
+++ src/main/java/com/ab/SpringBootMvcCiCd2Application.java
@@ -2,8 +2,10 @@ package com.ab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.ab.controllers","com.ab.configs"})
 public class SpringBootMvcCiCd2Application {
 
 	public static void main(String[] args) {
